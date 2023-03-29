2989b59abd81d545e8e362f50251f3e75a6f6083
Ryan Santiago <cr7rs7@gmail.com>
2023-03-29 01:59:19 -0400

    Initial Commit.

---
+++ src/_codux/boards/card/card.board.tsx
@@ -0,0 +1,20 @@
+import { createBoard } from '@wixc3/react-board';
+import { Card } from '../../../components/card/card';
+
+export default createBoard({
+    name: 'Card',
+    Board: () => (
+        <Card
+            movie={{
+                id: '123',
+                overview:
+                    'Kazakh TV talking head Borat is dispatched to the United States to report on the greatest country in the world. With a documentary crew in tow, Borat becomes more interested in locating and marrying Pamela Anderson.',
+                release_date: '10/22/1999',
+                title: 'Borat',
+                vote_average: 8,
+                poster_path:
+                    'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51RPHFy00cL._AC_UF894,1000_QL80_.jpg',
+            }}
+        />
+    ),
+});
---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,42 @@
+import styles from './menu.module.scss';
+import classNames from 'classnames';
+
+export interface MenuProps {
+    className?: string;
+}
+
+const sortBy: string[] = ['Popularity', 'Most Voted', 'Release Date'];
+const genre: string[] = ['Action', 'Comedy', 'Horror'];
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-menus-and-templates
+ */
+export const Menu = ({ className }: MenuProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.logo}>
+                <img src="https://i.imgur.com/zYa4iMN.png" className={styles.logoimg} />
+                <span className={styles.logotxt}>SantiMovie</span>
+            </div>
+            <span className={styles.title}>Sort By</span>
+            <hr className={styles.hr} />
+            <ul className={styles.list}>
+                {sortBy.map((item) => (
+                    <li className={styles.listItem} key={item}>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+            <span className={styles.title}>Genre</span>
+            <hr className={styles.hr} />
+            <ul className={styles.list}>
+                {genre.map((item) => (
+                    <li className={styles.listItem} key={item}>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
